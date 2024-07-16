0086a115ccb406aeade813b5b98eb622095e0df5
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-07-16 22:51:45 +0500

    done

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 
+let date = new Date()
+let yearChanger = document.querySelector("#yearChanger")
+yearChanger.innerText = `${date.getFullYear()}/${date.getMonth()}/${date.getDate()}`
+
 let img = document.querySelector("#img");
 let text = document.querySelector("#text");
 
 let showBulb = false
 function bulbOnOff(){
     
-    console.log(text.innerText);
+    // console.log(text.innerText);
     showBulb = !showBulb
     if(showBulb){
         img.src = "images/on.png"
